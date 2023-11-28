23fa74d0bbb5a3965c3072297c481cc601722912
BenjaminDevia <benjamin.devia1@mail.udp.cl>
2023-11-27 21:49:15 -0300

    fix get_tramos.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,10 @@
-# Tu lista original de valores
-valores = [4, 6, 2]
+from shapely.geometry import Point, LineString
 
-# Obtener una lista de índices ordenados según los valores
-indices_ordenados = sorted(range(len(valores)), key=lambda i: valores[i])
+# Definir un punto y una LineString de ejemplo
+point = Point(-70.688, -33.432)
+linestring = LineString([(-70.687, -33.433), (-70.686, -33.430), (-70.686, -33.425)])
 
-# Imprimir la lista de índices ordenados
-print(indices_ordenados)
+# Proyectar el punto sobre la LineString
+projected_point = linestring.interpolate(linestring.project(point))
+
+print(projected_point)
